0ce37764feb23cc39e10f4173cf56ba3903c363f
Amarildo <amarildop1dev@gmail.com>
2023-11-16 00:31:24 -0300

    Projeto

--- LabDevSoftware/urls.py
+++ LabDevSoftware/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    
+    path('', PaginaInicial.as_view(), name='index'),
+
 ]
+
+
--- LabDevSoftware/views.py
+++ LabDevSoftware/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
 # Create your views here.
+from django.views.generic import TemplateView
+
+
+class PaginaInicial(TemplateView):
+        template_name = 'index.html'
+
